653cf43c1418924c7c21660b5d9c683cfce558cd
NicoleCoding <nicole251097@gmail.com>
2024-08-31 21:17:39 +0300

    Created components for the collection page, finalized the form component for adding a book.

---
+++ src/components/Form.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+export default Form({onAddBook}) {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [pages, setPages] = useState('');
+    const [readStatus, setReadStatus] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onAddBook(title, author, pages, readStatus);
+        setTitle('');
+        setAuthor('');
+        setPages('');
+        setReadStatus(false);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>'
+            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" required />
+            <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} placeholder="Author" required />
+            <input type="number" value={pages} onChange={(e) => setPages(e.target.value)} placeholder="Pages" required />
+            <label>
+                <input type="checkbox" checked={read} onChange={(e) => setReadStatus(e.target.checked)} />
+                Read
+            </label>
+            <button type="submit">Add book</button>
+
+        
+        </form>
+    )
+
+}
