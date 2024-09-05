d1dc020124a6a607eff96f19d378709f9e983904
NicoleCoding <nicole251097@gmail.com>
2024-09-06 00:08:22 +0300

    - Added hero section on start page

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom"
+import Button from "../components/Button"
+
 export default function StartPage() {
     return (
-        <div>
-            <p>Discover your next favorite book and manage your reading collection effortlessly. </p>
+        <div id="container">
+            <section id="hero">
+                <p id="hero-text">Discover your next favorite book and manage your reading collection effortlessly. </p>
+                <Link to="/search">
+                <Button text="Find your next read"/>
+                </Link>
+            </section>
         </div>
     )
 }
