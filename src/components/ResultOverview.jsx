84d9668462bc65c8bc3a8751db21a06ae6b5d389
NicoleCoding <nicole251097@gmail.com>
2024-09-08 11:07:09 +0300

    - Made changes in the code, so that table, where the search results are shown,  is only visible once data has been fetched from the search

--- src/components/ResultOverview.jsx
+++ src/components/ResultOverview.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 
 export default function ResultOverview(props) {
-    return (
-        <table>
+  return (
+    <>
       {props.data.length > 0 && (
-        <>
+        <table>
           <thead>
             <tr>
               <th>Title</th>
@@ -13,25 +13,20 @@ export default function ResultOverview(props) {
             </tr>
           </thead>
           <tbody>
-            {props.data.slice(0, 10).map((queryItem, index) => {
-              return (
-                <tr key={index} onClick={() => props.displaySelectedBook(index)}>
-                  <td>{queryItem.title}</td>
-                  <td>
-                    {queryItem.author_name && queryItem.author_name.length > 0
-                      ? queryItem.author_name[0]
-                      : 'Unknown author'}
-                  </td>
-                  <td>{queryItem.first_publish_year}</td>
-                </tr>
-              );
-            })}
+            {props.data.slice(0, 10).map((queryItem, index) => (
+              <tr key={index} onClick={() => props.displaySelectedBook(index)}>
+                <td>{queryItem.title}</td>
+                <td>
+                  {queryItem.author_name && queryItem.author_name.length > 0
+                    ? queryItem.author_name[0]
+                    : "Unknown author"}
+                </td>
+                <td>{queryItem.first_publish_year}</td>
+              </tr>
+            ))}
           </tbody>
-        </>
+        </table>
       )}
-    </table>
-        
-
-    );
-    
+    </>
+  );
 }
