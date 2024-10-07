657321c646de23ec7ec50a92a67ca87d47be0b1f
NicoleCoding <nicole251097@gmail.com>
2024-10-07 10:35:31 +0300

    - Added media query for larger screens - Implemented hamburger menu for smaller screens - Adjusted styling of navigation for larger screens

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom";
+import Hamburger from "hamburger-react";
+import { useState } from "react";
 
 export default function Nav() {
+
+    const [isOpen, setOpen] = useState(false);
+
+
     return (
         <nav>
-            <Link className="link" to="/">Start</Link>
-            <Link className="link" to="/search">Search</Link>
-            <Link className="link" to="/collection">Book Collection</Link>
+            <div id="hamburger-container">
+                <Hamburger toggled={isOpen} toggle={setOpen} />
+            </div>
+            <ul className={`menu ${isOpen ? "open" : ""}`}>
+                <li><Link className="link" to="/">Start</Link></li>
+                <li><Link className="link" to="/search">Search</Link></li>
+                <li><Link className="link" to="/collection">Book Collection</Link></li>
+            </ul>
         </nav>
 
     );
