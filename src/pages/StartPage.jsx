828c024ba04c3a53e0050db11d82fea975f05390
NicoleCoding <nicole251097@gmail.com>
2024-10-09 15:18:07 +0300

    - Added classes for page headings and page intros for consistent fonts/color - Layout adjusted for smaller screens on the searchpage - Buttons styling adjusted for the smaller screens

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -3,15 +3,15 @@ import Button from "../components/Button"
 
 export default function StartPage() {
     return (
-        <div>
-            <section id="hero">
-                <h2 id="hero-heading">Welcome to Your Personal BookVault!</h2>
-                <p id="hero-text">Organize and track your reading journey with ease. Whether you are exploring new titles or managing your personal collection, we are here to help you discover, collect, and enjoy books.
+        <>
+            <main id="hero">
+                <h2 id="hero-heading" class="page-heading">Welcome to Your Personal BookVault!</h2>
+                <p id="hero-text" class="page-intro">Organize and track your reading journey with ease. Whether you are exploring new titles or managing your personal collection, we are here to help you discover, collect, and enjoy books.
                                   Get started by searching for your next read or checking your collection! </p>
                 <Link to="/search">
                 <Button text="Find your next read" className="primary-button"/>
                 </Link>
-            </section>
-        </div>
+            </main>
+        </>
     )
 }
